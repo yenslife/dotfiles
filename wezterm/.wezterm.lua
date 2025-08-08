7b886a09b78240d5835ee0f20bda8d59c961d714
yenslife <77geo5rge6@gmail.com>
2025-08-08 14:18:46 +0800

    Modify wezterm window border and style

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,11 +11,25 @@ config.initial_cols = 120
 config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 20
+config.font_size = 18
 config.color_scheme = "tokyonight_night"
 -- config.color_scheme = "tokyonight_day"
 
 config.font = wezterm.font("JetBrains Mono")
+
+-- Window style
+config.window_frame = {
+	active_titlebar_bg = "#1b1b1b",
+	inactive_titlebar_bg = "#1b1b1b",
+	-- 標題列文字顏色
+	active_titlebar_fg = "#c0c0c0",
+	inactive_titlebar_fg = "#555555",
+}
+
+config.window_padding = {
+	top = 0,
+	bottom = 0,
+}
 
 -- Finally, return the configuration to wezterm:
 return config
