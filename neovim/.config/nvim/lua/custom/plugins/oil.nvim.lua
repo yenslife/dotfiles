c8b06be39a4e755f6722cc64963d812671aa3ca2
yenslife <77geo5rge6@gmail.com>
2025-10-24 17:08:08 +0800

    Disable default keymaps for horizontal split and refresh actions

--- neovim/.config/nvim/lua/custom/plugins/oil.nvim.lua
+++ neovim/.config/nvim/lua/custom/plugins/oil.nvim.lua
@@ -6,6 +6,10 @@ return {
     view_options = {
       show_hidden = true, -- Show hidden files and directories
     },
+    keymaps = {
+      ['<C-h>'] = false, -- 移除原本的水平分割開啟快捷鍵
+      ['<C-l>'] = false, -- 移除"actions.refresh",
+    },
   },
   -- Optional dependencies
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
