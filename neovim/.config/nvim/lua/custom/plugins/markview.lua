6af2692ba4d924bd97123441db65452ad892b1d2
yenslife <77geo5rge6@gmail.com>
2026-06-14 05:45:09 +0800

    Add markdown image preview

--- neovim/.config/nvim/lua/custom/plugins/markview.lua
+++ neovim/.config/nvim/lua/custom/plugins/markview.lua
@@ -14,4 +14,15 @@ return {
       enable = true,
     },
   },
+
+  keys = {
+    -- 開/關 Markview render
+    { '<leader>mv', '<cmd>Markview<cr>', desc = '[M]arkview toggle [V]iew' },
+
+    -- 開/關 hybrid mode：游標附近顯示原始 Markdown，其他地方 render
+    { '<leader>mh', '<cmd>Markview HybridToggle<cr>', desc = '[M]arkview toggle [H]ybrid' },
+
+    -- 左右 split，一邊原文一邊預覽
+    { '<leader>ms', '<cmd>Markview splitToggle<cr>', desc = '[M]arkview [S]plit preview' },
+  },
 }
---
+++ neovim/.config/nvim/lua/custom/plugins/snacks.nvim.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    image = {
+      enabled = true,
+      doc = {
+        enabled = true,
+        inline = true,
+        -- conceal = false,
+        float = false,
+      },
+    },
+  },
+}
